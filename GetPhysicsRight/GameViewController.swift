8c60d20f6c9cb0de2deeb18e6dea0a7486729c78
Larry Nathaniel Mcdowell <nathaniel29651@gmail.com>
2020-03-06 12:18:35 -0500

    Initial commit of working wind from angles - ship weathervanes into the "wind", tapping rotates the wind by 20 degrees counter-clockwise, and ship weather vanes quickly into the new "wind"

--- GetPhysicsRight/GameViewController.swift
+++ GetPhysicsRight/GameViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 import QuartzCore
 import SceneKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, SCNSceneRendererDelegate {
 
+    var windAngleVariable:Float = -25
+    var ship:SCNNode!
+    var scene:SCNScene!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
@@ -41,10 +44,10 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(ambientLightNode)
         
         // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
         
+        scene.isPaused = false
         // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
@@ -57,13 +60,15 @@ class GameViewController: UIViewController {
         
         // show statistics such as fps and timing information
         scnView.showsStatistics = true
-        
+        scnView.isPlaying = true
+        scnView.delegate = self
         // configure the view
         scnView.backgroundColor = UIColor.black
-        
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
+        
+        ship.physicsBody?.momentOfInertia = SCNVector3Make(8, 8, 20)
     }
     
     @objc
@@ -83,23 +88,10 @@ class GameViewController: UIViewController {
             let material = result.node.geometry!.firstMaterial!
             
             // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
+
         }
+        
+        windAngleVariable += 20
     }
     
     override var shouldAutorotate: Bool {
@@ -118,4 +110,189 @@ class GameViewController: UIViewController {
         }
     }
 
+    
+    //MARK - ApplyForce Knowledge
+    
+    // force: and at: are both in local coordinate system
+     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+   
+        let rollForce = SCNVector3Make(0, 100, 0)
+        //let translatedRollForce = ship.presentation.convertVector(rollForce, to: scene.rootNode)
+
+        //ship.physicsBody?.applyForce(rollForce, at: SCNVector3Make(20, 0, 0), asImpulse: false)
+        
+        let pitchForce = SCNVector3Make(0, 100, 0)
+        //ship.physicsBody?.applyForce(pitchForce, at: SCNVector3Make(0, 0, 20), asImpulse: false)
+               
+  //MARK: -  wind from side
+        
+        var windForce = SCNVector3Make(300, 20, 0)
+        windForce = yRotateVector(forceVector: windForce, radians: windAngleVariable * .pi / 180)
+        
+        var angleBetweenWindAndShipForward = windForce.angleBetweenVectors(ship.presentation.worldFront)
+        if angleBetweenWindAndShipForward.isNaN {
+            angleBetweenWindAndShipForward = 0
+        }
+        windForce *= -cos(angleBetweenWindAndShipForward + .pi/2)
+        
+        print("strength: \(windForce.magnitude) at \(angleBetweenWindAndShipForward * 180 / .pi), z: \(renderer.pointOfView?.position.z)")
+        let translatedWindForce = scene.rootNode.convertVector(windForce, to: ship.presentation)
+        
+        var rudderForce = SCNVector3Make(100, 0, 0)
+        //ship.physicsBody?.applyForce(rudderForce, at: SCNVector3Make(0, 0, 20), asImpulse: false)
+        
+        let rudderLocation = SCNVector3Make(0, 0, 20)
+        let elevatorLocation = SCNVector3Make(0, 0, 19)
+        
+       
+        ship.physicsBody?.applyForce(translatedWindForce, at: rudderLocation, asImpulse: true)
+               
+      
+        renderer.pointOfView?.position.z = ship.presentation.position.z + 40
+        }
+    
+    func yRotateVector(forceVector:SCNVector3, radians:Float) -> SCNVector3{
+
+        let intVector = SCNVector3Make(forceVector.x * sin(radians) + forceVector.z * cos(radians), forceVector.y, forceVector.x * cos(radians) + forceVector.z * sin(radians) )
+        return intVector
+    }
+}
+
+
+extension SCNVector3 {
+    
+    /// Calculate the magnitude of this vector
+    var magnitude:SCNFloat {
+        get {
+            return sqrt(dotProduct(self))
+        }
+    }
+    
+    /// Vector in the same direction as this vector with a magnitude of 1
+    var normalized:SCNVector3 {
+        get {
+            let localMagnitude = magnitude
+            let localX = x / localMagnitude
+            let localY = y / localMagnitude
+            let localZ = z / localMagnitude
+            
+            return SCNVector3(localX, localY, localZ)
+        }
+    }
+    
+    /**
+     Calculate the dot product of two vectors
+     
+     - parameter vectorB: Other vector in the calculation
+     */
+    func dotProduct(_ vectorB:SCNVector3) -> SCNFloat {
+        
+        return (x * vectorB.x) + (y * vectorB.y) + (z * vectorB.z)
+    }
+    
+    /**
+     Calculate the dot product of two vectors
+     
+     - parameter vectorB: Other vector in the calculation
+     */
+    func crossProduct(_ vectorB:SCNVector3) -> SCNVector3 {
+        
+        let computedX = (y * vectorB.z) - (z * vectorB.y)
+        let computedY = (z * vectorB.x) - (x * vectorB.z)
+        let computedZ = (x * vectorB.y) - (y * vectorB.x)
+        
+        return SCNVector3(computedX, computedY, computedZ)
+    }
+    
+    /**
+     Calculate the angle between two vectors
+     
+     - parameter vectorB: Other vector in the calculation
+     */
+    func angleBetweenVectors(_ vectorB:SCNVector3) -> SCNFloat {
+        
+        //cos(angle) = (A.B)/(|A||B|)
+        let cosineAngle = (dotProduct(vectorB) / (magnitude * vectorB.magnitude))
+        return SCNFloat(acos(cosineAngle))
+    }
+    
+    func cosOfAngleBetweenVectors(_ vectorB:SCNVector3) -> SCNFloat {
+           
+           //cos(angle) = (A.B)/(|A||B|)
+           let cosineAngle = (dotProduct(vectorB) / (magnitude * vectorB.magnitude))
+           return cosineAngle
+       }
+}
+/**
+ Add two vectors
+ 
+ - parameter left: Addend 1
+ - parameter right: Addend 2
+ */
+func +(left:SCNVector3, right:SCNVector3) -> SCNVector3 {
+    
+    return SCNVector3(left.x + right.x, left.y + right.y, left.z + right.z)
+}
+
+/**
+ Subtract two vectors
+ 
+ - parameter left: Minuend
+ - parameter right: Subtrahend
+ */
+func -(left:SCNVector3, right:SCNVector3) -> SCNVector3 {
+    
+    return left + (right * -1.0)
+}
+
+/**
+ Add one vector to another
+ 
+ - parameter left: Vector to change
+ - parameter right: Vector to add
+ */
+func += (left: inout SCNVector3, right:SCNVector3) {
+    
+    left = SCNVector3(left.x + right.x, left.y + right.y, left.z + right.z)
+}
+
+/**
+ Subtract one vector to another
+ 
+ - parameter left: Vector to change
+ - parameter right: Vector to subtract
+ */
+func -= (left: inout SCNVector3, right:SCNVector3) {
+    
+    left = SCNVector3(left.x - right.x, left.y - right.y, left.z - right.z)
+}
+
+/**
+ Multiply a vector times a constant
+ 
+ - parameter vector: Vector to modify
+ - parameter constant: Multiplier
+ */
+func *(vector:SCNVector3, multiplier:SCNFloat) -> SCNVector3 {
+    
+    return SCNVector3(vector.x * multiplier, vector.y * multiplier, vector.z * multiplier)
+}
+
+/**
+ Multiply a vector times a constant and update the vector inline
+ 
+ - parameter vector: Vector to modify
+ - parameter constant: Multiplier
+ */
+func *= (vector: inout SCNVector3, multiplier:SCNFloat) {
+    
+    vector = vector * multiplier
+}
+
+func /= (vector: inout SCNVector3, divisor:SCNFloat){
+    if vector.x * vector.y * vector.z == 0 {
+        vector = SCNVector3Zero
+    }
+    
+    vector = SCNVector3(vector.x / divisor, vector.y / divisor, vector.z / divisor)
 }
